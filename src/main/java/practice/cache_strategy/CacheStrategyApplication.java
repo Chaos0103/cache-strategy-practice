16dff4b2f2a3a200bf0529b16cdb623858187602
Chaos0103 <lyt1228@naver.com>
2024-08-15 20:04:40 +0900

    init: 캐시 전략 연습 프로젝트 생성

---
+++ src/main/java/practice/cache_strategy/CacheStrategyApplication.java
@@ -0,0 +1,13 @@
+package practice.cache_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/cache_strategy/CacheStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.cache_strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
